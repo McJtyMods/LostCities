7b06ae61adc56d9cb265f1f701c8a290788d9eb4
McJty <mcjty1@gmail.com>
2022-09-20 14:21:47 +0200

    New notbreakable tag

--- src/main/java/mcjty/lostcities/worldgen/LostCityTerrainFeature.java
+++ src/main/java/mcjty/lostcities/worldgen/LostCityTerrainFeature.java
@@ -62,9 +62,6 @@ public class LostCityTerrainFeature {
     private boolean statesSetup = false;
     private static BlockState air;
     private static BlockState hardAir;
-    public static BlockState bedrock;
-    public static BlockState endportal;
-    public static BlockState endportalFrame;
 
     private BlockState liquid;
     private BlockState base;
@@ -211,9 +208,6 @@ public class LostCityTerrainFeature {
             base = profile.getBaseBlock();
             liquid = profile.getLiquidBlock();
 
-            bedrock = Blocks.BEDROCK.defaultBlockState();
-            endportal = Blocks.END_PORTAL.defaultBlockState();
-            endportalFrame = Blocks.END_PORTAL_FRAME.defaultBlockState();
             statesSetup = true;
         }
     }
