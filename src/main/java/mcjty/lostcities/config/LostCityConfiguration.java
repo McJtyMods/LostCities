4fd9cb943929970f4613f4fca89a270208bf99f1
strubium <113206902+strubium@users.noreply.github.com>
2023-04-24 09:51:57 -0500

    Update LostCityConfiguration.java

--- src/main/java/mcjty/lostcities/config/LostCityConfiguration.java
+++ src/main/java/mcjty/lostcities/config/LostCityConfiguration.java
@@ -171,7 +171,6 @@ public class LostCityConfiguration {
         profile.GENERATE_LAKES = false;
         profile.GENERATE_LIGHTING = true;
         profile.GENERATE_VILLAGES = false;
-//        profile.setIconFile("textures/gui/icon_default.png");
         standardProfiles.put(profile.getName(), profile);
 
         profile = new LostCityProfile("nodamage", true);
@@ -398,7 +397,6 @@ public class LostCityConfiguration {
         profile.VINE_CHANCE = 0.1f;
         profile.EXPLOSION_CHANCE = 0;
         profile.MINI_EXPLOSION_CHANCE = 0;
-//        profile.MINI_EXPLOSION_MAXRADIUS = 10; <--- why is this here?
         profile.RUBBLELAYER = true;
         profile.RUBBLE_DIRT_SCALE = 2.0f;
         profile.RUBBLE_LEAVE_SCALE = 2.0f;
@@ -451,6 +449,8 @@ public class LostCityConfiguration {
         profile.setDescription("Realistic worldgen (similar to Quark's)");
         profile.setIconFile("textures/gui/icon_realistic.png");
         profile.GENERATOR_OPTIONS = "{\"coordinateScale\":175.0,\"heightScale\":75.0,\"lowerLimitScale\":512.0,\"upperLimitScale\":512.0,\"depthNoiseScaleX\":200.0,\"depthNoiseScaleZ\":200.0,\"depthNoiseScaleExponent\":0.5,\"mainNoiseScaleX\":165.0,\"mainNoiseScaleY\":106.61267,\"mainNoiseScaleZ\":165.0,\"baseSize\":8.267606,\"stretchY\":13.387607,\"biomeDepthWeight\":1.2,\"biomeDepthOffset\":0.2,\"biomeScaleWeight\":3.4084506,\"biomeScaleOffset\":0.0,\"seaLevel\":63,\"useCaves\":true,\"useDungeons\":true,\"dungeonChance\":7,\"useStrongholds\":true,\"useVillages\":true,\"useMineShafts\":true,\"useTemples\":true,\"useMonuments\":true,\"useRavines\":true,\"useWaterLakes\":true,\"waterLakeChance\":49,\"useLavaLakes\":true,\"lavaLakeChance\":80,\"useLavaOceans\":false,\"fixedBiome\":-1,\"biomeSize\":4,\"riverSize\":5,\"dirtSize\":33,\"dirtCount\":10,\"dirtMinHeight\":0,\"dirtMaxHeight\":256,\"gravelSize\":33,\"gravelCount\":8,\"gravelMinHeight\":0,\"gravelMaxHeight\":256,\"graniteSize\":33,\"graniteCount\":10,\"graniteMinHeight\":0,\"graniteMaxHeight\":80,\"dioriteSize\":33,\"dioriteCount\":10,\"dioriteMinHeight\":0,\"dioriteMaxHeight\":80,\"andesiteSize\":33,\"andesiteCount\":10,\"andesiteMinHeight\":0,\"andesiteMaxHeight\":80,\"coalSize\":17,\"coalCount\":20,\"coalMinHeight\":0,\"coalMaxHeight\":128,\"ironSize\":9,\"ironCount\":20,\"ironMinHeight\":0,\"ironMaxHeight\":64,\"goldSize\":9,\"goldCount\":2,\"goldMinHeight\":0,\"goldMaxHeight\":32,\"redstoneSize\":8,\"redstoneCount\":8,\"redstoneMinHeight\":0,\"redstoneMaxHeight\":16,\"diamondSize\":8,\"diamondCount\":1,\"diamondMinHeight\":0,\"diamondMaxHeight\":16,\"lapisSize\":7,\"lapisCount\":1,\"lapisCenterHeight\":16,\"lapisSpread\":16}";
+        profile.GENERATE_MANSIONS = false;
+        profile.GENERATE_OCEANMONUMENTS = false;
         standardProfiles.put(profile.getName(), profile);
 
         profile = new LostCityProfile("water_empty", false);
