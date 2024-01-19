db976f3d806fe5afcbd9286ebdef3882248cd940
McJty <mcjty1@gmail.com>
2024-01-19 09:19:47 +0100

    Reduced default multi-building max to 2/2

--- src/main/java/mcjty/lostcities/config/LostCityProfile.java
+++ src/main/java/mcjty/lostcities/config/LostCityProfile.java
@@ -150,8 +150,8 @@ public class LostCityProfile implements ILostCityProfile {
     public float BRIDGE_CHANCE = .7f;
     public float FOUNTAIN_CHANCE = .05f;
     public float BUILDING2X2_CHANCE = .03f;
-    public int MULTI_MAX_X = 3;
-    public int MULTI_MAX_Z = 3;
+    public int MULTI_MAX_X = 2;
+    public int MULTI_MAX_Z = 2;
     public boolean BRIDGE_SUPPORTS = true;
 
     public int BEDROCK_LAYER = 1;
