70fbc5933ca8c64a8f81193487c4e5746f5392ca
TqLxQuanZ <quanpro777@hotmail.com>
2024-03-04 21:03:12 +0800

    Added park elevation and park border block options.

--- src/main/java/mcjty/lostcities/worldgen/LostCityTerrainFeature.java
+++ src/main/java/mcjty/lostcities/worldgen/LostCityTerrainFeature.java
@@ -1995,7 +1995,10 @@ public class LostCityTerrainFeature {
                         driver.block(elevation).incZ();
                     }
                 }
-                height++;
+                if (info.profile.PARK_ELEVATION)
+                {
+                    height++;
+                }
             }
 
             switch (streetType) {
@@ -2366,10 +2369,10 @@ public class LostCityTerrainFeature {
                             }
                         }
                         if (b == null) {
-                            b = compiledPalette.get(street);
+                            b = info.profile.PARK_BORDER ? compiledPalette.get(street) : grass.get();
                         }
                     } else {
-                        b = compiledPalette.get(street);
+                        b = info.profile.PARK_BORDER ? compiledPalette.get(street) : grass.get();
                     }
                 } else {
                     b = grass.get();
