7ed814724477efc3f6ea681b3ec2a387fab21998
McJty <mcjty1@gmail.com>
2024-06-19 06:46:05 +0200

    Fixed a problem in the worldstyle codec

--- src/main/java/mcjty/lostcities/gui/NullDimensionInfo.java
+++ src/main/java/mcjty/lostcities/gui/NullDimensionInfo.java
@@ -106,7 +106,6 @@ public class NullDimensionInfo implements IDimensionInfo {
                 Optional.empty(),
                 Optional.empty(),
                 Collections.emptyList(),
-                Optional.empty(),
                 Optional.empty()
         ));
         this.seed = seed;
