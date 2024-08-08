b764f825047b1c16289b5bacc0c5d116b26f22dc
McJty <mcjty1@gmail.com>
2024-08-08 06:36:14 +0200

    - Better error reporting in some situations - Highway parts can now be randomized

--- src/main/java/mcjty/lostcities/worldgen/lost/regassets/data/HighwayParts.java
+++ src/main/java/mcjty/lostcities/worldgen/lost/regassets/data/HighwayParts.java
@@ -1,24 +1,50 @@
 package mcjty.lostcities.worldgen.lost.regassets.data;
 
+import com.mojang.datafixers.util.Either;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
-public record HighwayParts(String tunnel, String open, String bridge, String tunnelBi, String openBi, String bridgeBi) {
+public record HighwayParts(List<String> tunnel, List<String> open, List<String> bridge, List<String> tunnelBi, List<String> openBi, List<String> bridgeBi) {
 
     public static final Codec<HighwayParts> CODEC = RecordCodecBuilder.create(instance ->
             instance.group(
-                    Codec.STRING.optionalFieldOf("tunnel", "highway_tunnel").forGetter(HighwayParts::tunnel),
-                    Codec.STRING.optionalFieldOf("open", "highway_open").forGetter(HighwayParts::open),
-                    Codec.STRING.optionalFieldOf("bridge", "highway_bridge").forGetter(HighwayParts::bridge),
-                    Codec.STRING.optionalFieldOf("tunnel_bi", "highway_tunnel_bi").forGetter(HighwayParts::tunnelBi),
-                    Codec.STRING.optionalFieldOf("open_bi", "highway_open_bi").forGetter(HighwayParts::openBi),
-                    Codec.STRING.optionalFieldOf("bridge_bi", "highway_bridge_bi").forGetter(HighwayParts::bridgeBi)
+                    Codec.either(Codec.STRING, Codec.STRING.listOf())
+                            .optionalFieldOf("tunnel", Either.left("highway_tunnel"))
+                            .xmap(either -> either.map(List::of, Function.identity()), list -> list.size() == 1 ? Either.left(list.get(0)) : Either.right(list))
+                            .forGetter(HighwayParts::tunnel),
+                    Codec.either(Codec.STRING, Codec.STRING.listOf())
+                            .optionalFieldOf("open", Either.left("highway_open"))
+                            .xmap(either -> either.map(List::of, Function.identity()), list -> list.size() == 1 ? Either.left(list.get(0)) : Either.right(list))
+                            .forGetter(HighwayParts::open),
+                    Codec.either(Codec.STRING, Codec.STRING.listOf())
+                            .optionalFieldOf("bridge", Either.left("highway_bridge"))
+                            .xmap(either -> either.map(List::of, Function.identity()), list -> list.size() == 1 ? Either.left(list.get(0)) : Either.right(list))
+                            .forGetter(HighwayParts::bridge),
+                    Codec.either(Codec.STRING, Codec.STRING.listOf())
+                            .optionalFieldOf("tunnel_bi", Either.left("highway_tunnel_bi"))
+                            .xmap(either -> either.map(List::of, Function.identity()), list -> list.size() == 1 ? Either.left(list.get(0)) : Either.right(list))
+                            .forGetter(HighwayParts::tunnelBi),
+                    Codec.either(Codec.STRING, Codec.STRING.listOf())
+                            .optionalFieldOf("open_bi", Either.left("highway_open_bi"))
+                            .xmap(either -> either.map(List::of, Function.identity()), list -> list.size() == 1 ? Either.left(list.get(0)) : Either.right(list))
+                            .forGetter(HighwayParts::openBi),
+                    Codec.either(Codec.STRING, Codec.STRING.listOf())
+                            .optionalFieldOf("bridge_bi", Either.left("highway_bridge_bi"))
+                            .xmap(either -> either.map(List::of, Function.identity()), list -> list.size() == 1 ? Either.left(list.get(0)) : Either.right(list))
+                            .forGetter(HighwayParts::bridgeBi)
             ).apply(instance, HighwayParts::new));
 
-    public static final HighwayParts DEFAULT = new HighwayParts("highway_tunnel", "highway_open", "highway_bridge",
-            "highway_tunnel_bi", "highway_open_bi", "highway_bridge_bi");
+    public static final HighwayParts DEFAULT = new HighwayParts(
+            List.of("highway_tunnel"),
+            List.of("highway_open"),
+            List.of("highway_bridge"),
+            List.of("highway_tunnel_bi"),
+            List.of("highway_open_bi"),
+            List.of("highway_bridge_bi"));
 
     public Optional<HighwayParts> get() {
         if (this == DEFAULT) {
@@ -28,15 +54,15 @@ public record HighwayParts(String tunnel, String open, String bridge, String tun
         }
     }
 
-    public String tunnel(boolean bi) {
+    public List<String> tunnel(boolean bi) {
         return bi ? tunnelBi : tunnel;
     }
 
-    public String open(boolean bi) {
+    public List<String> open(boolean bi) {
         return bi ? openBi : open;
     }
 
-    public String bridge(boolean bi) {
+    public List<String> bridge(boolean bi) {
         return bi ? bridgeBi : bridge;
     }
 }
