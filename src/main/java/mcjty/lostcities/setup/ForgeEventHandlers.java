091d7be6c1fd5e4287b85c93af28af9bb98b14fe
TqLxQuanZ <yeezhanquan1998@gmail.com>
2026-01-02 11:42:05 +0800

    Merged from 1.20

--- src/main/java/mcjty/lostcities/setup/ForgeEventHandlers.java
+++ src/main/java/mcjty/lostcities/setup/ForgeEventHandlers.java
@@ -23,6 +23,7 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
@@ -59,7 +60,6 @@ public class ForgeEventHandlers {
     public void commandRegister(RegisterCommandsEvent event) {
         ModCommands.register(event.getDispatcher());
     }
-
 
     @SubscribeEvent
     public void onPlayerLoggedIn(PlayerEvent.PlayerLoggedInEvent event) {
@@ -216,16 +216,12 @@ public class ForgeEventHandlers {
                     if (needsCheck) {
                         BlockPos pos = findSafeSpawnPoint(serverLevel, dimensionInfo, isSuitable, event.getSettings());
                         serverLevel.setDefaultSpawnPos(pos, 0.0f);
-                        event.getSettings().setSpawn(pos, 0.0f);
-                        spawnPositions.put(serverLevel.dimension(), pos);
                         event.setCanceled(true);
                     }
                 }
                 case FLOATING, SPACE, CAVERN, CAVERNSPHERES -> {
                     BlockPos pos = findSafeSpawnPoint(serverLevel, dimensionInfo, isSuitable, event.getSettings());
                     serverLevel.setDefaultSpawnPos(pos, 0.0f);
-                    event.getSettings().setSpawn(pos, 0.0f);
-                    spawnPositions.put(serverLevel.dimension(), pos);
                     event.setCanceled(true);
                 }
             }
