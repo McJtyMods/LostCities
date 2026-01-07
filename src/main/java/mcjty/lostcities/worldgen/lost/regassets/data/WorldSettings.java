c9d6ed77fc67a1c65140065946150883983465ce
McJty <mcjty1@gmail.com>
2026-01-07 06:35:54 +0100

    Extended the world settings with four new options: vinewest, vineeast, vinesouth, and vinenorth. With these you can change the vines that are used for the buildings

--- src/main/java/mcjty/lostcities/worldgen/lost/regassets/data/WorldSettings.java
+++ src/main/java/mcjty/lostcities/worldgen/lost/regassets/data/WorldSettings.java
@@ -3,10 +3,21 @@ package mcjty.lostcities.worldgen.lost.regassets.data;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.VineBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
-public record WorldSettings(RailwayAvoidance railwayAvoidance, int railPartHeight6) {
+public record WorldSettings(
+        RailwayAvoidance railwayAvoidance,
+        int railPartHeight6,
+        BlockState vineWest,
+        BlockState vineEast,
+        BlockState vineSouth,
+        BlockState vineNorth) {
 
     public enum RailwayAvoidance implements StringRepresentable {
         IGNORE("ignore"),
@@ -27,10 +38,18 @@ public record WorldSettings(RailwayAvoidance railwayAvoidance, int railPartHeigh
     public static final Codec<WorldSettings> CODEC = RecordCodecBuilder.create(instance ->
             instance.group(
                     StringRepresentable.fromEnum(RailwayAvoidance::values).fieldOf("railwayavoidance").forGetter(l -> l.railwayAvoidance),
-                    Codec.INT.optionalFieldOf("railpartheight6", 1).forGetter(l -> l.railPartHeight6)
+                    Codec.INT.optionalFieldOf("railpartheight6", 1).forGetter(l -> l.railPartHeight6),
+                    BlockState.CODEC.optionalFieldOf("vinewest", getVine(VineBlock.WEST)).forGetter(l -> l.vineWest),
+                    BlockState.CODEC.optionalFieldOf("vineeast", getVine(VineBlock.EAST)).forGetter(l -> l.vineEast),
+                    BlockState.CODEC.optionalFieldOf("vinesouth", getVine(VineBlock.SOUTH)).forGetter(l -> l.vineSouth),
+                    BlockState.CODEC.optionalFieldOf("vinenorth", getVine(VineBlock.NORTH)).forGetter(l -> l.vineNorth)
             ).apply(instance, WorldSettings::new));
 
-    public static final WorldSettings DEFAULT = new WorldSettings(RailwayAvoidance.IGNORE, 1);
+    public static final WorldSettings DEFAULT = new WorldSettings(RailwayAvoidance.IGNORE, 1, getVine(VineBlock.WEST), getVine(VineBlock.EAST), getVine(VineBlock.SOUTH), getVine(VineBlock.NORTH));
+
+    private static @NotNull BlockState getVine(BooleanProperty property) {
+        return Blocks.VINE.defaultBlockState().setValue(property, true);
+    }
 
     public Optional<WorldSettings> get() {
         if (this == DEFAULT) {
