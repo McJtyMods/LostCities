9a38b5606481388856ef70864229892c000b6f54
TqLxQuanZ <yeezhanquan1998@gmail.com>
2026-01-08 15:30:26 +0800

    7.4.11 - Fixed an issue where cache cleaning cause bridge generation stuck.

--- src/main/java/mcjty/lostcities/worldgen/lost/BuildingInfo.java
+++ src/main/java/mcjty/lostcities/worldgen/lost/BuildingInfo.java
@@ -1348,7 +1348,8 @@ public class BuildingInfo implements ILostChunkInfo {
         xBridgeType = bt;
         // Here we can automatically mark the rest of the bridge as ok. Saves on calculation
         i = i.getXmin();
-        while (i != minimum) {
+        ChunkCoord minCoord = minimum.coord;
+        while (!i.coord.equals(minCoord)) {
             i.xBridgeType = bt;
             i.xBridgeTypeCalculated = true;
             i.zBridgeType = null;
@@ -1416,7 +1417,8 @@ public class BuildingInfo implements ILostChunkInfo {
         zBridgeType = bt;
         // Here we can automatically mark the rest of the bridge as ok. Saves on calculation
         i = i.getZmin();
-        while (i != minimum) {
+        ChunkCoord minCoord = minimum.coord;
+        while (!i.coord.equals(minCoord)) {
             i.zBridgeType = bt;
             i.zBridgeTypeCalculated = true;
             i.xBridgeType = null;
