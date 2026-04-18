f11750f05c12aa606e8ca50ad41b65cbd17fded7
TqLxQuanZ <yeezhanquan1998@gmail.com>
2026-04-18 21:16:35 +0800

    More tweaks.

--- src/main/java/mcjty/lostcities/worldgen/lost/regassets/BuildingPartRE.java
+++ src/main/java/mcjty/lostcities/worldgen/lost/regassets/BuildingPartRE.java
@@ -46,7 +46,7 @@ public class BuildingPartRE implements IAsset<BuildingPartRE> {
         for (List<String> slice : slices) {
             StringBuilder builder = new StringBuilder();
             for (String s : slice) {
-                builder.append(s.intern());
+                builder.append(s);
             }
             this.slices[idx++] = builder.toString();
         }
