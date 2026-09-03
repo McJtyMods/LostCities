cfc36dd97caf5705b4df71f5e64c25e85a1c82c7
McJty <mcjty1@gmail.com>
2026-09-03 08:49:59 +0200

    Fix config screen button rendering

--- src/main/java/mcjty/lostcities/gui/elements/ButtonExt.java
+++ src/main/java/mcjty/lostcities/gui/elements/ButtonExt.java
@@ -1,16 +1,20 @@
 package mcjty.lostcities.gui.elements;
 
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.PlainTextButton;
 import net.minecraft.client.gui.components.Tooltip;
-import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 
-public class ButtonExt extends PlainTextButton {
+public class ButtonExt extends Button {
 
     public ButtonExt(int x, int y, int w, int h, Component message, OnPress action) {
-        super(x, y, w, h, message, action, Minecraft.getInstance().font);
+        super(x, y, w, h, message, action, DEFAULT_NARRATION);
+    }
+
+    @Override
+    protected void extractContents(GuiGraphicsExtractor graphics, int mouseX, int mouseY, float partialTick) {
+        extractDefaultSprite(graphics);
+        extractDefaultLabel(graphics.textRendererForWidget(this, GuiGraphicsExtractor.HoveredTextEffects.NONE));
     }
 
     public ButtonExt tooltip(Component tooltip) {
